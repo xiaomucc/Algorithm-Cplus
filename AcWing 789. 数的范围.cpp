204e1b477371a34055971003488cd9bae112db76
xiaomu <95511105+xiaomucc@users.noreply.github.com>
2022-11-22 21:08:28 +0800

    Create AcWing 789. 数的范围.cpp

---
+++ AcWing 789. 数的范围.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+const int N = 100010;
+int nums[N];
+int n, m, x;
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> nums[i];
+	while (m--)
+	{
+		cin >> x;
+		int l = 0, r = n - 1;
+		while (l < r)
+		{
+			int mid = l + ((r - l) >> 1);
+			if (nums[mid] >= x) r = mid;
+			else l = mid + 1;
+		}
+		if (nums[l] != x) cout << "-1 -1" << endl;
+		else
+		{
+			cout << l << " ";
+			l = 0, r = n - 1;
+			while (l < r)
+			{
+				int mid = l + ((r - l + 1) >> 1);
+				if (nums[mid] <= x) l = mid;
+				else r = mid - 1;
+			}
+			cout << l << endl;
+		}
+	}
+	return 0;
+}
